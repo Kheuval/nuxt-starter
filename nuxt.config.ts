6afc03c00eba908b0c89def894164d1e5d344d4a
Kheuval <hadrien.hartstein@gmail.com>
2024-07-15 09:32:59 +0200

    [Feature] Update Nuxt to 3.12

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,10 +4,13 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@pinia/nuxt", "@nuxt/test-utils/module"],
   css: ["~/assets/css/main.css"],
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+
+  compatibilityDate: "2024-07-15",
 });
